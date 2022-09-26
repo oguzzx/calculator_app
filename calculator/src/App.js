94da9933fc97b3860332b6f7232b94fa6d798102
oguzzx <1204225052@nku.edu.tr>
2022-09-26 17:26:07 +0300

    calculator_app
    hesapmakinesi uygulaması

---
+++ calculator/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import * as math from "mathjs";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + ""]);
+  };
+
+  const calculateResult = () => {
+    const input = text.join("");
+    setResult(math.evaluate(input));
+  };
+
+  const resetInput = () => {
+    setText("");
+    setResult("");
+  };
+  return (
+    <div className="App">
+      <div className="calc-wrapper">
+        <Input text={text} result={result} />
+        <div className="row">
+          <Button symbol="7" handleClick={addToText} />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
+          <Button symbol="/" color="#00ad85" handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
+          <Button symbol="*" color="#00ad85" handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
+          <Button symbol="+" color="#00ad85" handleClick={addToText} />
+        </div>
+        <div className="row">
+          <Button symbol="0" handleClick={addToText} />
+          <Button symbol="." handleClick={addToText} />
+          <Button symbol="=" handleClick={calculateResult} />
+          <Button symbol="-" color="#00ad85" handleClick={addToText} />
+        </div>
+        <Button handleClick={resetInput} symbol="CLEAR" color="red" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
